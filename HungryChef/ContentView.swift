51060d57b38f908cee3044945aa945eaad41b3dd
arshaansayed <arshaan.h.sayed@gmail.com>
2022-06-15 17:29:10 -0400

    Initial Commit

---
+++ HungryChef/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HungryChef
+//
+//  Created by Arshaan Sayed on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HungryChef/HungryChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryChefApp.swift
+//  HungryChef
+//
+//  Created by Arshaan Sayed on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct HungryChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
